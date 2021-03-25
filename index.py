c3e09081f1366e2154fe175bfb971e286d2c114e
Cangyu <jefflin.je598@gmail.com>
2021-03-25 10:02:10 +0800

    add automatic uuid update

--- index.py
+++ index.py
@@ -3,16 +3,18 @@ from flask import Flask, redirect, url_for, render_template, request
 from flask_mysqldb import MySQL
 from werkzeug.utils import secure_filename
 import uuid
+import glob
 
 # Some format and sizes limits
-UPLOAD_FOLDER = 'imgs/'
-ALLOWED_EXTENSIONS = set(['png', 'jpg'])
+UPLOAD_FOLDER = '/imgs/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 MAX_CONTENT_LENGTH = 10*1024*1024   # 10MB
 
 # Actual initialization of web interface
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = os.getcwd() + UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = MAX_CONTENT_LENGTH
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 #MySQL
 app.config["MYSQL_HOST"] = "localhost"
@@ -38,21 +40,21 @@ def getUUID(email):
     cur = mysql.connection.cursor()
     cur.execute(sql)
     data = cur.fetchall()
-    return data[0]
+    return data
 
 def getPic1(UUID):
-    sql = "SELECT PIC_1 FROM Photos WHERE UUID="+str(UUID)
+    sql = "SELECT PIC_1 FROM Photos WHERE UUID='"+str(UUID)+"'"
     cur =mysql.connection.cursor()
     cur.execute(sql)
     data = cur.fetchall()
-    return data[0]
+    return data[0][0]
 
 def getPic2(UUID):
-    sql = "SELECT PIC_2 FROM Photos WHERE UUID="+str(UUID)
+    sql = "SELECT PIC_2 FROM Photos WHERE UUID='"+str(UUID)+"'"
     cur =mysql.connection.cursor()
     cur.execute(sql)
     data = cur.fetchall()
-    return data[0]
+    return data[0][0]
     
 
 def update(UUID,img1, img2):
@@ -75,36 +77,52 @@ def index():
             email = request.values['email']
             img1 = request.files['img1']
             img2 = request.files['img2']
+            img1_p = ""
+            img2_p = ""
             uuid_usr = request.values['UUID']
+            # print(uuid_usr)
+            # print(getUUID(email))
             # Check whether the data is existed
             query_r = query(email)
-            if query == None:
+            if query_r == ():
+                print("no uuid")
                 # Save image1
                 if img1 and allowed_file(img1.filename):
-                    filename = secure_filename(str(uuid.uuid5())+"."+img1.filename.rsplit('.', 1)[1])
-                    print(filename)
+                    filename = secure_filename(str(uuid.uuid4())+"."+img1.filename.rsplit('.', 1)[1])
+                    img1_p = filename
                     img1.save(os.path.join(app.config['UPLOAD_FOLDER'], 
                                        filename))
                 # Save image2
-                if img2 and allowed_file():
-                    filename = secure_filename(str(uuid.uuid5())+"."+img2.filename.rsplit('.', 1)[1])
-                    print(filename)
+                if img2 and allowed_file(img2.filename):
+                    filename = secure_filename(str(uuid.uuid4())+"."+img2.filename.rsplit('.', 1)[1])
                     img2.save(os.path.join(app.config['UPLOAD_FOLDER'],
                                        filename))
-                insert((str(uuid.uuid5()),email,img1,img2))
-            elif uuid_usr == getUUID(email):
+                    img2_p = filename
+                insert((str(uuid.uuid4()),email,img1_p,img2_p))
+            elif uuid_usr == getUUID(email)[0][0]:
+                print("enter UUID")
+                print(img1)
+                print(img2)
                 # Save image1
+                
                 if img1 and allowed_file(img1.filename):
-                    filename = secure_filename(getPic1(uuid_usr)+"."+img1.filename.rsplit('.', 1)[1])
-                    print(filename)
+                    print("removing img1")
+                    os.remove(app.config['UPLOAD_FOLDER']+str(getPic1(uuid_usr)))
+                    print(app.config['UPLOAD_FOLDER']+str(getPic1(uuid_usr)))
+                    filename = secure_filename(str(getPic1(uuid_usr)).rsplit('.',1)[0]+"."+img1.filename.rsplit('.', 1)[1])
                     img1.save(os.path.join(app.config['UPLOAD_FOLDER'], 
                                        filename))
+                    img1_p=filename
                 # Save image2
-                if img2 and allowed_file():
-                    filename = secure_filename(getPic2(uuid_usr)+"."+img2.filename.rsplit('.', 1)[1])
-                    print(filename)
+                if img2 and allowed_file(img2.filename):
+                    print("removing img2")
+                    os.remove(app.config['UPLOAD_FOLDER']+str(getPic2(uuid_usr)))
+                    print(app.config['UPLOAD_FOLDER']+str(getPic2(uuid_usr)))
+                    filename = secure_filename(str(getPic2(uuid_usr)).rsplit('.',1)[0]+"."+img2.filename.rsplit('.', 1)[1])
                     img2.save(os.path.join(app.config['UPLOAD_FOLDER'],
-                update(uuid_usr, img1, img2) 
+                                       filename))
+                    img2_p=filename
+                update(uuid_usr, img1_p, img2_p)
     return render_template("form.html")
 
 if __name__ == '__main__':
